84034dbaab51c721a9c5605ec50f6db1e0d5f3e8
Mija Cruz <mija.cruz.tarazona@gmail.com>
2023-08-04 03:51:17 -0500

    solucion de errores del formulario

--- formulario.js
+++ formulario.js
@@ -1,13 +1,12 @@
 function mostrar() {
     var nom = document.form1.nombre.value;
-    var apll = document.form1.apellidos.value;
     var ed = document.form1.edad.value;
+    var num = document.form1.celular.value;
     var dr = document.form1.direccion.value;
     var gm = document.form1.correo.value;
     var pg = document.form1.pago.value;
-    var num = document.form1.celular.value;
 
-    if (nom == "" || apll == "" || ed == "" || dr == "" || gm == "" || pg == "" || num == "") {
+    if (nom == "" || ed == "" || dr == "" || gm == "" || pg == "" || num == "") {
         alert('Faltan algunos datos por completar');
     } else {
         alert('¡Su compra fue hecha con exito! \n Pronto llegará su pedido a casa');
